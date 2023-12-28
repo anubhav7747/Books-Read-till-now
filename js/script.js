21509d7e69119fdfef33d7d6cc725d63f2f1163b
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-12-29 00:13:02 +0530

    index, style, script

--- js/script.js
+++ js/script.js
@@ -28,8 +28,36 @@ randomQuote5 = () => {
     document.getElementById('quote2').innerText = '— François Mauriac';
 }
 
-setTimeout(randomQuote1, 5000);
-setTimeout(randomQuote2, 10000);
-setTimeout(randomQuote3, 15000);
-setTimeout(randomQuote4, 20000);
-setTimeout(randomQuote5, 25000);
+randomQuote6 = () => {
+    document.getElementById('quote1').innerHTML = '"Books and doors are the same thing. You open them, and you go through into another world."';
+    document.getElementById('quote2').innerText = '— Jeanette Winterson';
+}
+
+
+const functionArray = [randomQuote1, randomQuote2, randomQuote3, randomQuote4, randomQuote5, randomQuote6];
+
+getRandomFunction = (array) => {
+    // Generate a random index within the array length
+    const randomIndex = Math.floor(Math.random() * array.length);
+  
+    // Access and return the function at the random index
+    return array[randomIndex];
+}
+  
+callRandomFunctionInLoop = () => {
+    const randomFunction = getRandomFunction(functionArray);
+    randomFunction();
+  
+    // Set a timeout to call the function again after a delay (e.g., 4000 milliseconds)
+    setTimeout(callRandomFunctionInLoop, 4000);
+}
+  
+// Start the infinite loop
+callRandomFunctionInLoop();
+
+
+// setTimeout(randomQuote1, 4000);
+// setTimeout(randomQuote2, 8000);
+// setTimeout(randomQuote3, 12000);
+// setTimeout(randomQuote4, 16000);
+// setTimeout(randomQuote5, 20000);
